dc6a768b77b9093d4f671526b11ab7134ac8006d
enriqueSFranco <enrique.sfranco04@gmail.com>
2023-07-13 17:30:06 -0600

    feat:remove build

--- vite.config.ts
+++ vite.config.ts
@@ -5,18 +5,6 @@ import path from 'node:path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  build: {
-    rollupOptions: {
-      // https://rollupjs.org/guide/en/#outputmanualchunks
-      output: {
-        manualChunks: {
-          'group-builder': [
-            './src/views/Builder'
-          ]
-        }
-      }
-    }
-  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
