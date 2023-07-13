c2565b193116cad8926c6e2c70a64b161c6c0f6a
enriqueSFranco <enrique.sfranco04@gmail.com>
2023-07-13 12:26:00 -0600

    feat: Add chunks for lazy loading

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,18 @@ import path from 'node:path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      // https://rollupjs.org/guide/en/#outputmanualchunks
+      output: {
+        manualChunks: {
+          'group-builder': [
+            './src/views/Builder'
+          ]
+        }
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
