d8af6189d26c5acfa79ac647a8064cb0fe5eab50
enriqueSFranco <enrique.sfranco04@gmail.com>
2023-07-13 07:27:21 -0600

    feat: Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,18 @@
 <template>
   <main class="w-full h-2/3 flex items-center justify-center">
-    <div class='grid grid-cols-1 place-items-center gap-4 text-md px-10 z-20'>
+    <div class='grid grid-cols-1 place-items-center gap-10 text-md px-10 z-20'>
       <h1 class='text-5xl font-extrabold font-marker lg:text-7xl'>
         cv easy
       </h1>
-      <p class="tracking-wide">¡Haz que tu currículum destaque y conquista nuevas oportunidades laborales en cuestión de
+      <p class="tracking-wide block w-2/3 text-center">¡Haz que tu currículum destaque y conquista nuevas oportunidades
+        laborales en
+        cuestión de
         minutos!</p>
       <div class="relative group">
         <div
           class="absolute -inset-0.5 bg-gradient-to-r from-pink-600 to-purple-600 blur opacity-75 rounded-lg leading-none">
         </div>
-        <button class="relative text-lg w-40 rounded p-3 capitalize z-20 bg-slate-950 rounded-lg">Comenzar</button>
+        <button class="relative text-lg w-40 p-3 capitalize z-20 bg-slate-950 rounded-lg">Comenzar</button>
       </div>
     </div>
   </main>
