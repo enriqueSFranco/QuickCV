d93a9051cbabc9bb9cd8af5a2b5a1e4bc06e8496
enriqueSFranco <enrique.sfranco04@gmail.com>
2023-07-13 17:35:18 -0600

    feat: Add FormPersonalInfo

---
+++ src/components/Form/FormPersonalInfo.vue
@@ -0,0 +1,37 @@
+<template>
+  <form class="w-full flex flex-col gap-2">
+    <BaseInput type="text" :label="'Nombre'" v-model="user.name" />
+    <BaseInput type="text" :label="'Apellidos'" v-model="user.lastName" />
+    <BaseInput type="text" :label="'Puesto'" v-model="user.job" />
+    <BaseInput type="email" :label="'Correo electronico'" v-model="user.email" />
+  </form>
+  <footer>
+    <nav>
+      <router-link :to="{ name: 'builder' }">volver</router-link>
+      <router-link :to="{ name: 'contact-forms' }">siguiente</router-link>
+    </nav>
+  </footer>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive } from 'vue'
+import { PersonalInformation } from '@/shared/types.d'
+import BaseInput from '@/components/Input/BaseInput.vue'
+
+export default defineComponent({
+  name: 'FormPersonalInfo',
+  components: {
+    BaseInput
+  },
+  setup () {
+    const user: PersonalInformation = reactive({
+      name: '',
+      lastName: '',
+      job: '',
+      email: ''
+    })
+
+    return { user }
+  }
+})
+</script>
