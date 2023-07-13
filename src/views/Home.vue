b140e195835bc60571f8237f209bb91315f2b5b9
enriqueSFranco <enrique.sfranco04@gmail.com>
2023-07-12 19:28:22 -0600

    feat: Home page

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <main class="w-full h-2/3 flex items-center justify-center">
+    <div class='grid grid-cols-1 place-items-center gap-4 text-md px-10 z-20'>
+      <h1 class='text-5xl font-extrabold font-marker lg:text-7xl'>
+        cv easy
+      </h1>
+      <p class="tracking-wide">¡Haz que tu currículum destaque y conquista nuevas oportunidades laborales en cuestión de
+        minutos!</p>
+      <div class="relative group">
+        <div
+          class="absolute -inset-0.5 bg-gradient-to-r from-pink-600 to-purple-600 blur opacity-75 rounded-lg leading-none">
+        </div>
+        <button class="relative text-lg w-40 rounded p-3 capitalize z-20 bg-slate-950 rounded-lg">Comenzar</button>
+      </div>
+    </div>
+  </main>
+</template>
