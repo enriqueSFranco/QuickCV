5fc330e96896e4792bffe7cf61482f3811bc1e1e
enriqueSFranco <enrique.sfranco04@gmail.com>
2023-07-13 17:31:39 -0600

    feat: Add type PersonalInformation

--- src/shared/types.d.ts
+++ src/shared/types.d.ts
@@ -4,3 +4,14 @@ export interface SectionCv {
   summary: string
   href: string
 }
+
+// FORMULARIO [INFORMACION PERSONAL]
+export interface People {
+  name: string
+  lastName: string
+}
+
+export interface PersonalInformation extends People {
+  job: string
+  email: string
+}
