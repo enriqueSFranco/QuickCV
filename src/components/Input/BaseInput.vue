d6ab5592108a5189a2d64226873eaa96895ce4db
enriqueSFranco <enrique.sfranco04@gmail.com>
2023-07-13 17:35:32 -0600

    feat: Add base input

---
+++ src/components/Input/BaseInput.vue
@@ -0,0 +1,23 @@
+<template>
+  <label v-if="label">{{ label }}</label>
+  <input v-bind="$attrs" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)"
+    class="p-3 outline-none border-none rounded-lg bg-slate-800">
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'BaseInput',
+  props: {
+    label: {
+      type: String,
+      default: '',
+    },
+    modelValue: {
+      type: [String, Number],
+      default: ''
+    }
+  }
+})
+</script>
