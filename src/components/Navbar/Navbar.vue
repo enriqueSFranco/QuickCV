cb7d35ef877df64c62bc46f7c1e2f8be6dd34803
enriqueSFranco <enrique.sfranco04@gmail.com>
2023-07-12 19:28:46 -0600

    feat: Navbar component

---
+++ src/components/Navbar/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+  <header class='w-full h-16 flex items-center justify-end px-7'>
+    <label for='theme' class='bg-slate-400/20 p-2 rounded relative w-10 h-10'>
+      <!-- {theme ? <IconSun /> : <IconMoon />} -->
+      <input type='checkbox' name='theme' class='absolute w-full h-full top-0 left-0 appearance-none cursor-pointer' />
+    </label>
+  </header>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Navbar'
+}
+</script>
