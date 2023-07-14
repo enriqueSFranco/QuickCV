acf1a578a9bd62f324bcbfe136a48454f3e62e2d
enriqueSFranco <enrique.sfranco04@gmail.com>
2023-07-13 19:11:01 -0600

    feat: Add forms

--- src/components/Form/FormPersonalInfo.vue
+++ src/components/Form/FormPersonalInfo.vue
@@ -1,14 +1,22 @@
 <template>
-  <form class="w-full flex flex-col gap-2">
-    <BaseInput type="text" :label="'Nombre'" v-model="user.name" />
-    <BaseInput type="text" :label="'Apellidos'" v-model="user.lastName" />
-    <BaseInput type="text" :label="'Puesto'" v-model="user.job" />
-    <BaseInput type="email" :label="'Correo electronico'" v-model="user.email" />
+  <form class="w-full flex flex-col gap-2 text-sm">
+    <BaseInput type="text" :placeholder="'Carlos Enrique'" :label="'Nombre'" v-model="form.name" />
+    <BaseInput type="text" :placeholder="'Salinas Franco'" :label="'Apellidos'" v-model="form.lastName" />
+    <BaseInput type="text" :placeholder="'Frontend Developer'" :label="'Puesto'" v-model="form.job" />
+    <BaseInput type="email" :placeholder="'kike@gmail.com'" :label="'Correo electronico'" v-model="form.email" />
   </form>
-  <footer>
+  <!-- <footer>
     <nav>
-      <router-link :to="{ name: 'builder' }">volver</router-link>
-      <router-link :to="{ name: 'contact-forms' }">siguiente</router-link>
+    </nav>
+  </footer> -->
+  <footer class="w-full flex flex-grow items-end">
+    <nav class="w-full flex items-center">
+      <div class="w-full grid grid-cols-2 justify-items-end">
+        <router-link :to="{ name: 'builder' }"
+          class="bg-slate-800 w-28 px-3 py-2 rounded-sm grid place-items-center capitalize">volver</router-link>
+        <router-link :to="{ name: 'contact-forms' }"
+          class="bg-slate-800 w-28 px-3 py-2 rounded-sm grid place-items-center capitalize">siguiente</router-link>
+      </div>
     </nav>
   </footer>
 </template>
@@ -24,14 +32,14 @@ export default defineComponent({
     BaseInput
   },
   setup () {
-    const user: PersonalInformation = reactive({
+    const form: PersonalInformation = reactive({
       name: '',
       lastName: '',
       job: '',
       email: ''
     })
 
-    return { user }
+    return { form }
   }
 })
 </script>
