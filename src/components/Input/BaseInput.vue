4a3c96a8fbc357cb02113c00e4b34b6275ac9d41
enriqueSFranco <enrique.sfranco04@gmail.com>
2023-07-13 19:16:50 -0600

    feat: Add components base (input, button, textarea)

--- src/components/Input/BaseInput.vue
+++ src/components/Input/BaseInput.vue
@@ -1,7 +1,10 @@
 <template>
-  <label v-if="label">{{ label }}</label>
-  <input v-bind="$attrs" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)"
-    class="p-3 outline-none border-none rounded-lg bg-slate-800">
+  <div class="w-full flex flex-col gap-2">
+    <label v-if="label">{{ label }}</label>
+    <input v-bind="$attrs" :placeholder="placeholder" :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+      class="p-3 outline-none border-none rounded-lg bg-slate-800 w-full">
+  </div>
 </template>
 
 <script lang="ts">
@@ -13,6 +16,10 @@ export default defineComponent({
     label: {
       type: String,
       default: '',
+    },
+    placeholder: {
+      type: String,
+      default: ''
     },
     modelValue: {
       type: [String, Number],
---
+++ src/components/TextArea/BaseTextArea.vue
@@ -0,0 +1,21 @@
+<template>
+  <textarea name="" id="" cols="30" rows="10" :placeholder="placeholder"></textarea>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'BaseTextArea',
+  props: {
+    modelValue: {
+      type: String,
+      default: ''
+    },
+    placeholder: {
+      type: String,
+      default: ''
+    }
+  }
+})
+</script>
