fc2bde9eae87760362e76b68ae29a9539ae56d35
enriqueSFranco <enrique.sfranco04@gmail.com>
2023-07-13 18:01:01 -0600

    feat(ui): Add layout

--- src/layouts/Layout.vue
+++ src/layouts/Layout.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="h-screen w-full dark:bg-slate-950 text-slate-50">
+  <div class="w-full h-screen dark:bg-slate-950 text-slate-50">
     <Navbar />
     <slot></slot>
   </div>
@@ -15,3 +15,5 @@ export default {
   }
 }
 </script>
+
+<style scoped></style>
