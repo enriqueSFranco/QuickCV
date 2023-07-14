4e715c22432d2ea972fb0e52fab0dedfe66be472
enriqueSFranco <enrique.sfranco04@gmail.com>
2023-07-13 19:17:08 -0600

    feat: Add layout view page

---
+++ src/layouts/LayoutView.vue
@@ -0,0 +1,29 @@
+<template>
+  <section>
+    <header class="w-full flex flex-col gap-2">
+      <h2 class="font-bold capitalize [&>p]:tracking-wide">{{ titleView }}</h2>
+      <p>{{ subtitleView }}</p>
+    </header>
+    <slot></slot>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'LayoutView',
+  props: {
+    titleView: {
+      type: String,
+      default: '',
+      required: true
+    },
+    subtitleView: {
+      type: String,
+      default: '',
+      required: true
+    }
+  }
+})
+</script>
