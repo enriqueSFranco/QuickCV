47f54fad5c010829d6bf37388171f8d5bb06d50f
enriqueSFranco <enrique.sfranco04@gmail.com>
2023-07-17 16:11:32 -0600

    feat: uso de pinia

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import { router } from './router'
 import './style.css'
 
 const $app = createApp(App)
+
+const pinia = createPinia()
+
+// use pinia
+$app.use(pinia)
 
 // use vue-router
 $app.use(router)
