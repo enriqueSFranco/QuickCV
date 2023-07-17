ceaaa1120622192c43e19027e49c0f22b3ae3eb1
enriqueSFranco <enrique.sfranco04@gmail.com>
2023-07-17 16:14:25 -0600

    feat(ui): Preview Curriculum

--- src/components/Curriculum/Curriculum.vue
+++ src/components/Curriculum/Curriculum.vue
@@ -1,3 +1,19 @@
 <template>
   <h2>Curriculum</h2>
+  <p>{{ store.formData.name }}</p>
+  <p>{{ store.formData.lastName }}</p>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { usePersonalInformation } from '@/stores/personalInformation'
+
+export default defineComponent({
+  name: 'Curriculum',
+  setup () {
+    const store = usePersonalInformation()
+
+    return { store }
+  }
+})
+</script>
