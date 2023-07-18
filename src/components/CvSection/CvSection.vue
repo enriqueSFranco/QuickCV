72ec9b9b2e38979d398bdf359a09475b97d1a5aa
enriqueSFranco <enrique.sfranco04@gmail.com>
2023-07-18 15:34:40 -0600

    feat: Se quito el router-link

--- src/components/CvSection/CvSection.vue
+++ src/components/CvSection/CvSection.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="w-full flex justify-between items-center">
+  <div class="w-full h-auto my-4 flex justify-between items-center gap-4">
     <div class="w-full h-full flex flex-col justify-between gap-1">
       <h2 class="uppercase font-bold">{{ payload.title }}</h2>
       <p class="tracking-wide text-slate-200">{{ payload.summary }}</p>
     </div>
-    <router-link :to="{ name: payload.href }">
-      <img :src="arrowRight" alt="icon-arrow-right" />
-    </router-link>
+    <img :src="arrowRight" alt="icon-arrow-right" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, PropType } from 'vue'
-import arrowRight from '@/assets/icons/arrow-right-square.svg'
+import arrowRight from '@/assets/icons/arrow-right.svg'
 import { SectionCv } from '@/shared/types.d'
 
 export default defineComponent({
