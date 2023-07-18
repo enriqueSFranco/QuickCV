d405b9cac2e56650c3c4f7ed95216ecb0dda0b75
enriqueSFranco <enrique.sfranco04@gmail.com>
2023-07-17 18:14:56 -0600

    feat(ui): se agrego un boton para descargar el pdf

--- src/components/Curriculum/Curriculum.vue
+++ src/components/Curriculum/Curriculum.vue
@@ -1,7 +1,12 @@
 <template>
-  <h2>Curriculum</h2>
-  <p>{{ store.formData.name }}</p>
-  <p>{{ store.formData.lastName }}</p>
+  <section class="w-full h-full flex flex-col items-center border border-slate-800 rounded-lg p-4">
+    <h2>Curriculum</h2>
+    <p>{{ store.formData.name }}</p>
+    <p>{{ store.formData.lastName }}</p>
+    <footer class="flex flex-grow items-end justify-center">
+      <button>descargar curriculum</button>
+    </footer>
+  </section>
 </template>
 
 <script lang="ts">
