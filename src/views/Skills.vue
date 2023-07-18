3f08a9bf32eb90e916ec79fe4481b6875b661e96
enriqueSFranco <enrique.sfranco04@gmail.com>
2023-07-18 15:31:36 -0600

    feat(view): Add view Skills

---
+++ src/views/Skills.vue
@@ -0,0 +1,20 @@
+<template>
+  <LayoutView :title-view="'Destaca tu identidad profesional'"
+    :subtitle-view="'No olvides resaltar tus habilidades tanto en el ámbito interpersonal como profesional, y mencionar las herramientas y tecnologías que dominas'">
+    <FormSkills />
+  </LayoutView>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import LayoutView from '@/layouts/LayoutView.vue'
+import FormSkills from '@/components/Form/FormSkills.vue'
+
+export default defineComponent({
+  name: 'Skills',
+  components: {
+    LayoutView,
+    FormSkills
+  }
+})
+</script>
