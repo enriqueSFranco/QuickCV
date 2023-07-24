047de53ee325fd926a8be9a160198d591ee5d172
enriqueSFranco <enrique.sfranco04@gmail.com>
2023-07-24 13:49:52 -0600

    feat: función para renderizar la pagina del cv

--- src/helpers/pageviewer.ts
+++ src/helpers/pageviewer.ts
@@ -1,43 +1,90 @@
-import type { ProfessionalProfile, PersonalInformation, Website } from '@/shared/types.d'
+import type { ProfessionalProfile, PersonalInformation, Website, Education } from '@/shared/types.d'
+import { drawText, splitTextIntoLines } from './drawText'
+import { pdfjs } from './pdfjs'
 
 interface Data {
   personalInformation: PersonalInformation
   websites: Website[]
   professionalProfile: ProfessionalProfile
+  // education: Education[]
 }
 
-interface Props {
-  text: Data
-}
+export function pageviewer (pdfUrl: string, data: Data, onRender): void {
+  const { personalInformation, websites, professionalProfile } = data
+  const scale = 0.8
+  const pdfFile = pdfjs.getDocument({ data: pdfUrl })
+  void pdfFile.promise.then((pdf) => {
+    void pdf.getPage(1).then(page => {
+      const viewport = page.getViewport({ scale })
+      const outputScale = window.devicePixelRatio ?? 1
 
-export function pageviewer (data: Props): void {
-  // Prepare canvas using PDF page dimensions
-  const canvas = window.document.getElementById('the-canvas')
-  if (canvas instanceof HTMLCanvasElement) {
-    const context = canvas.getContext('2d')
-    if (context != null) {
-      context.font = '20px Arial'
-      context.fillStyle = '#fff'
-      const mainText = data.text.personalInformation.email
-      const mainTextWidth = context.measureText(mainText).width
-      context.clearRect(0, 0, canvas.width, canvas.height)
-      context.fillText(`${data.text.personalInformation.name} ${data.text.personalInformation.lastName}`, 10, 16)
-      context.fillText(data.text.personalInformation.job, 10, 40)
-      context.fillText(data.text.personalInformation.email, 10, 64)
-      context.fillText(data.text.professionalProfile.summary, 10, 88)
+      // preparando el canvas
+      const canvas = document.getElementById('the-canvas')
+      if (canvas instanceof HTMLCanvasElement) {
+        const context = canvas.getContext('2d')
+        if (context != null) {
+          canvas.width = Math.floor(viewport.width * outputScale)
+          canvas.height = Math.floor(viewport.height * outputScale)
+          canvas.style.width = `${Math.floor(viewport.width)}px`
+          canvas.style.height = `${Math.floor(viewport.height)}px`
 
-      // FORMAS DE CONTACTO
-      const websites = data.text.websites
-      const y = 64 // posicion del email en el eje y
-      const lineHeight = 20 // espacio entre las lineas
-      let currentY = y
-      websites.forEach(website => {
-        const url = website.url
-        const positionX = mainTextWidth + 20
-        context.fillText(website.url, 350, 64)
-        context.fillText(url, positionX, currentY)
-        currentY += lineHeight
-      })
-    }
-  }
+          const renderContext = {
+            canvasContext: context,
+            viewport
+          }
+          void page.render(renderContext).promise.then(() => {
+            onRender()
+
+            // CONSTANTES
+            context.fillStyle = '#000' // Color del texto
+            const x = 20 // posición inicial en el ejeX de los elementos dentro del canva
+            const fontTitle = 'bold 26px Arial'
+
+            // INFORMACION PERSONAL
+            const fullnameHeight = drawText({ ctx: context, font: fontTitle, text: `${personalInformation.name} ${personalInformation.lastName}`, x: 20, y: 50 })
+            const jobHeight = drawText({ ctx: context, font: 'bold 24px Arial', text: `${personalInformation.job}`, x, y: 85 })
+            const emailHeight = drawText({ ctx: context, font: '24px Arial', text: `${personalInformation.email}`, x, y: 120 })
+
+            // WEBSITES
+            const initialY = 120 // posición del elemento email en el ejeY en el canva
+            let posX = context.measureText(personalInformation.email).width // ancho del texto email
+            for (const website of websites) {
+              const { url } = website
+              const urlWitdth = context.measureText(url).width // ancho de cada url
+              drawText({
+                ctx: context,
+                text: url,
+                font: '24px Arial',
+                x: posX + 40,
+                y: initialY
+              })
+              posX += urlWitdth + 30
+            }
+            let height = fullnameHeight + jobHeight + emailHeight + 150
+            // EDUCACIÓN
+            const educationTitleHeight = drawText({ ctx: context, font: fontTitle, text: 'Educación', x, y: height })
+
+            height += educationTitleHeight + 100
+            // PERFIL PROFESIONAL
+            drawText({ ctx: context, font: fontTitle, text: 'Perfil Profesional', x, y: height })
+
+            const lines = splitTextIntoLines({ ctx: context, txt: professionalProfile.summary, maxWidth: canvas.width - 20 })
+            let professionalProfilePosY = height + 35
+            // let lineHeightCurrent = context.measureText(lines[0]).width
+            for (let i = 0; i < lines.length; i++) {
+              const currentLine = lines[i]
+              drawText({
+                ctx: context,
+                font: '26px Arial',
+                text: `${currentLine}`,
+                x,
+                y: professionalProfilePosY
+              })
+              professionalProfilePosY += 35
+            }
+          })
+        }
+      }
+    })
+  })
 }
