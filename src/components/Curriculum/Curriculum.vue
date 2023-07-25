93f0659d33ec77d858827ba4754cbb93eea1445f
enriqueSFranco <enrique.sfranco04@gmail.com>
2023-07-24 19:10:45 -0600

    feat: componentes

--- src/components/Curriculum/Curriculum.vue
+++ src/components/Curriculum/Curriculum.vue
@@ -1,6 +1,11 @@
 <template>
   <section class="w-full h-full bg-slate-100 flex flex-col items-center p-4">
-    <canvas id="the-canvas" class="bg-slate-50 shadow-lg opacity-70 rounded"></canvas>
+    <template v-if="!loading">
+      <!-- TODO: implementar componente de carga -->
+      <div>cargando vista previa</div>
+    </template>
+    <canvas v-else id="the-canvas" class="shadow-lg rounded w-full h-full"> </canvas>
+    <button>hola</button>
     <footer class="w-full flex flex-grow items-end justify-center">
       <button class="bg-slate-800 text-slate-50 rounded-full font-medium p-4">Descargar curriculum</button>
     </footer>
@@ -9,16 +14,18 @@
 
 <script lang="ts">
 import { defineComponent, onMounted } from 'vue'
+import { storeToRefs } from 'pinia'
 import { usePreviewCurriculum } from '@/stores/previewCV'
 
 export default defineComponent({
   name: 'Curriculum',
   setup () {
     const previewCV = usePreviewCurriculum()
+    const { loading } = storeToRefs(previewCV)
     onMounted(async () => {
       previewCV.renderPDF()
     })
-
+    return { loading }
   }
 })
 </script>
--- src/stores/previewCV.ts
+++ src/stores/previewCV.ts
@@ -3,21 +3,24 @@ import { defineStore } from 'pinia'
 import { STORE_NAME } from '@/shared/types.d'
 import { usePersonalInformation, useProfessinalProfile, useWebsite } from '.'
 import { pageviewer } from '@/helpers/pageviewer'
+import { useEducation } from './education'
 
 export const usePreviewCurriculum = defineStore(STORE_NAME.PREVIEW_CURRICULUM, () => {
   const personalInformation = usePersonalInformation()
   const websites = useWebsite()
   const professionalProfile = useProfessinalProfile()
+  const education = useEducation()
   const data = reactive({
     personalInformation: personalInformation.formData,
     websites: websites.formData,
-    professionalProfile: professionalProfile.formData
+    professionalProfile: professionalProfile.formData,
+    education: education.formData
   })
   const loading: Ref<boolean> = ref(true)
   const pdfFile: Ref<string | null> = ref(null)
 
   watch(personalInformation, () => {
-    console.log('cambio en la informacion')
+    void renderPDF()
   }, { deep: true })
 
   async function renderPDF (): Promise<void> {
