324f1f5ec55ea5d5704fd35cf29f8a308d345f68
enriqueSFranco <enrique.sfranco04@gmail.com>
2023-07-24 18:54:38 -0600

    feat: add prop icon

--- src/views/ContactForms.vue
+++ src/views/ContactForms.vue
@@ -1,11 +1,13 @@
 <template>
   <LayoutView :title-view="'formas de contacto'"
-    :subtitle-view="'Incorpora enlaces estratégicos a tus plataformas en línea, incluyendo tu sitio web personal, perfil de LinkedIn o cuentas de redes sociales profesionales.'">
+    :subtitle-view="'Incorpora enlaces estratégicos a tus plataformas en línea, incluyendo tu sitio web personal, perfil de LinkedIn o cuentas de redes sociales profesionales.'"
+    :icon="iconContactForms">
     <FormContactForms />
   </LayoutView>
 </template>
 
 <script setup lang="ts">
 import FormContactForms from '@/components/Form/FormContactForms.vue'
-import LayoutView from '@/layouts/LayoutView.vue';
+import iconContactForms from '@/assets/icons/linked.svg'
+import LayoutView from '@/layouts/LayoutView.vue'
 </script>
