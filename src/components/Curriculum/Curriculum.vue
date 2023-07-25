82d5f9fc749fbe64e5f9e1dcf1a914d2a8267188
enriqueSFranco <enrique.sfranco04@gmail.com>
2023-07-25 15:38:18 -0600

    feat(ui): implement Loader

--- src/components/Curriculum/Curriculum.vue
+++ src/components/Curriculum/Curriculum.vue
@@ -1,11 +1,8 @@
 <template>
-  <section class="w-full h-full bg-slate-100 flex flex-col items-center p-4">
-    <template v-if="!loading">
-      <!-- TODO: implementar componente de carga -->
-      <div>cargando vista previa</div>
-    </template>
+  <section class="w-full h-full bg-slate-100 flex flex-col items-center gap-4 p-4">
+    <Loader v-if="loading" :text="'cargando vista previa'" />
     <canvas v-else id="the-canvas" class="shadow-lg rounded w-full h-full"> </canvas>
-    <button>hola</button>
+    <!-- TODO: implementar <button>hola</button> -->
     <footer class="w-full flex flex-grow items-end justify-center">
       <button class="bg-slate-800 text-slate-50 rounded-full font-medium p-4">Descargar curriculum</button>
     </footer>
@@ -16,9 +13,11 @@
 import { defineComponent, onMounted } from 'vue'
 import { storeToRefs } from 'pinia'
 import { usePreviewCurriculum } from '@/stores/previewCV'
+import Loader from '@/components/Loader/Loader.vue'
 
 export default defineComponent({
   name: 'Curriculum',
+  components: { Loader },
   setup () {
     const previewCV = usePreviewCurriculum()
     const { loading } = storeToRefs(previewCV)
