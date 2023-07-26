16fd3443cc771e151e0b8cdfa575e7cc200e0313
enriqueSFranco <enrique.sfranco04@gmail.com>
2023-07-26 13:19:52 -0600

    feat: Add constants

---
+++ src/shared/constants.d.ts
@@ -0,0 +1,10 @@
+export const FONTS = {
+  fontTitle: 'bold 30px Arial',
+  fontSubtitle: 'bold 26px Arial',
+  fontText: '26px Arial'
+
+}
+
+export const COORDINATES = {
+  x: 20 // posición inicial en el ejeX de los elementos dentro del canva
+}
