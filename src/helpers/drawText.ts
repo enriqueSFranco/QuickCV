8b190205b812bfdc70890f1101e087853454763a
enriqueSFranco <enrique.sfranco04@gmail.com>
2023-07-26 13:19:31 -0600

    feat: Add helpers

--- src/helpers/drawText.ts
+++ src/helpers/drawText.ts
@@ -13,28 +13,3 @@ export function drawText ({ ctx, text, font, colorText = '#000', x, y }: DrawTex
   ctx.fillText(text, x, y)
   return ctx.measureText(text).actualBoundingBoxAscent + ctx.measureText(text).actualBoundingBoxDescent
 }
-
-export function splitTextIntoLines ({ ctx, txt, maxWidth }: { ctx: CanvasRenderingContext2D, txt: string, maxWidth: number }): string[] {
-  // hola soy enrique
-  const words = txt.split(' ') // ['hola', 'soy', 'enrique']
-  let currentLine = words[0] // ['hola']
-  const lines = []
-
-  for (let i = 1; i < words.length; i++) {
-    const word = words[i]
-    const currentLineWidth = ctx.measureText(currentLine + ' ' + word).width
-    // si el ancho del texto es menor al maxWidth, entonces seguimos concatenado las palabras
-    if (currentLineWidth < maxWidth) {
-      currentLine += ' ' + word
-    } else {
-      lines.push(currentLine)
-      currentLine = word
-    }
-  }
-  lines.push(currentLine)
-  return lines
-}
-
-export function calculateWidthTetx ({ ctx, txt }: { ctx: CanvasRenderingContext2D, txt: string }): number {
-  return ctx.measureText(txt).width
-}
