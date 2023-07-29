ee64c2ab7ec494dc7fd28ec7709f11e89e33831c
enriqueSFranco <enrique.sfranco04@gmail.com>
2023-07-28 18:30:34 -0600

    feat: componentes

--- src/components/Loader/Loader.vue
+++ src/components/Loader/Loader.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="shadow-lg rounded w-[476px] h-full grid place-items-center bg-black/5 opacity-75">
+  <div class="shadow-lg rounded w-[476px] h-full grid place-items-center bg-slate-50 opacity-75">
     <div class="w-2/3 relative grid place-items-center">
-      <h2 class="text-gray-900 font-semibold">{{ text }}</h2>
+      <h2 class="text-gray-900 font-light">{{ text }}</h2>
       <div
         class="relative w-2/3 h-2 overflow-hidden after:absolute after: top-3 left-0 after:w-full after:h-2 after:rounded-lg after:bg-gray-900 after:animate-progress">
       </div>
