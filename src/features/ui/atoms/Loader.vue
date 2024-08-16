94706f91045f1a56575403fc75ca65b9505be44a
enriqueSFranco <enrique.sfranco04@gmail.com>
2024-08-16 17:44:41 -0600

    feat(features/ui/atoms): add components

---
+++ src/features/ui/atoms/Loader.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="shadow-lg rounded w-[476px] h-full grid place-items-center bg-slate-50 opacity-75">
+    <div class="w-2/3 relative grid place-items-center">
+      <h2 class="text-gray-900 font-light">{{ text }}</h2>
+      <div
+        class="relative w-2/3 h-2 overflow-hidden after:absolute after: top-3 left-0 after:w-1/2 after:h-2 after:rounded-lg after:bg-gray-800 after:animate-progress">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Loader',
+  props: {
+    text: {
+      type: String,
+      default: 'cargando contenido'
+    }
+  }
+})
+</script>
