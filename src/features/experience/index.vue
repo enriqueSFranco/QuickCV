bbda98548827e86e002968f13fa48c3f3a1a855f
enriqueSFranco <enrique.sfranco04@gmail.com>
2024-08-16 18:08:11 -0600

    feat(features): add screens

---
+++ src/features/experience/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <LayoutView :title-view="'Destaca tu identidad profesional'"
+    :subtitle-view="'Comparte tu trayectoria laboral, incluyendo el cargo que ocupaste, el nombre de la empresa, el período de tiempo y una breve descripción de las responsabilidades que tuviste.'">
+    <FormExperience />
+  </LayoutView>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import LayoutView from '@/layouts/LayoutView.vue'
+import FormExperience from '@/components/Form/experience.vue'
+
+export default defineComponent({
+  name: 'Experience',
+  components: {
+    LayoutView,
+    FormExperience
+  }
+})
+</script>
---
+++ src/features/personal-info/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <LayoutView :title-view="'Destaca tu identidad profesional'"
+    :subtitle-view="'Ingresa tus datos personales para facilitar la comunicación con los reclutadores'">
+    <FormPersonalInfo />
+  </LayoutView>
+</template>
+
+<script setup lang="ts">
+import FormPersonalInfo from '@/components/ui/personal-info.vue'
+import LayoutView from '@/layouts/LayoutView.vue'
+</script>
