b8fc59e46f2474ca8f18ca94c33b35ff05f4ab12
enriqueSFranco <enrique.sfranco04@gmail.com>
2024-08-16 18:27:41 -0600

    add enums and models

---
+++ src/shared/enums.d.ts
@@ -0,0 +1,8 @@
+// ENUMS
+export enum STORE_NAME {
+  HEADER = 'Header',
+  PROFILE = 'Profile',
+  EXPERIENCE = 'Experience',
+  EDUCATION = 'Education',
+  Knowledge = 'Knowledge'
+}
---
+++ src/shared/models.d.ts
@@ -0,0 +1,33 @@
+export interface HeaderCv {
+  name: string
+  lastname: string
+  fullname: string
+  job: string
+  summary: string
+  phone: string
+  socialNetworks: string[]
+}
+
+export interface ProfileCv { summary: string }
+
+export interface EducationCv {
+  id: number
+  school: string
+  degree: string
+  dateInit: string
+  dateEnd: string
+}
+
+export interface ExperienceCv {
+  id: number
+  company: string
+  job: string
+  dateInit: string
+  dateEnd: string
+  summary: string
+}
+
+export interface KnowledgeCv {
+  id: number
+  skill: string
+}
